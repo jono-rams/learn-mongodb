7a2a5cc7a0e2efcecc8f5516a9d0080588eb7b5d
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-10 10:40:07 -0400

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// db connection
+let db;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+    db = getDb();
+  }
 });
 
 // routes
