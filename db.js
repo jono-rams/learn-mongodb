0dd6921bdd1679860ecf19c347226db93d90d3e8
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-10 13:35:09 -0400

    MongoDB Atlas

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+// get environment variables
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
 
 let dbConnection;
+let uri = `mongodb+srv://${db_username}:${db_password}@cluster0.ugkk0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+    MongoClient.connect(uri)
       .then((client) => {
-        dbConnection = client.db();
+        dbConnection = client.db("bookstore");
         return cb();
       })
       .catch((err) => {
